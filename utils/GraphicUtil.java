6165ea01b9826d53f54f424810efed702bef8ed9
github-lizy <35718630+github-lizy@users.noreply.github.com>
2021-08-03 17:57:47 +0800

    Update GraphicUtil.java
    update GraphicUtil:
    add method printRGBA
    add method printInt

--- utils/GraphicUtil.java
+++ utils/GraphicUtil.java
@@ -1,1 +1,19 @@
+public class GraphicUtil {
+  
+  // Java 通过color 的int值计算出a,r,g,b的值
+  public static void printRGBA(int color) {
+    int alpha = color >>> 24;
+    int r = ( color & 0xff0000 ) >> 16;
+    int g = ( color & 0xff00 ) >> 8;
+    int b = color & 0xff;
+    System.out.println("int color is " + color + "  ARGB = " + alpha + ", " + r + ", " + g + ", " + b);
+    printInt(alpha, r, g, b);
+  }
+
+  //通过a,r,g,b的值计算color的int值
+  public static void printInt(int alpha, int r, int g, int b) {
+    int color = (alpha << 24) | (r << 16) | (g << 8) | b;
+    System.out.println("color = "+color);
+  }
+}
 
